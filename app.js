187e1e4940970a2eeb9325730eea56a066cf0673
GGuede <gast22498@gmail.com>
2022-08-25 21:21:56 -0300

    sweet alert

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let Carrito = []
 
 //--------------------------------Constructor----------------------------------//
 class Monitor {
-    constructor(id,marca, pulgadas, definicion, precio,nombre,img,cantidad) {
+    constructor(id,marca, pulgadas, definicion, precio,nombre,img) {
       this.id = id
       this.marca = marca
       this.pulgadas = pulgadas
@@ -13,27 +13,26 @@ class Monitor {
       this.precio = precio
       this.nombre = nombre
       this.img = img
-      this.cantidad = cantidad
     }
   }
 
 //--------------------------------------Objetos del Array-------------------------//
-const monitor1 = new Monitor(1,"COMSTAR", 27, 720, 8000,"Monitor Comstar 220","./Multimedia/Monitor1.jpg",1)
+const monitor1 = new Monitor(1,"COMSTAR", 27, 720, 8000,"Monitor Comstar 220","./Multimedia/Monitor1.jpg")
 Monitores.push(monitor1)
 
-const monitor2 = new Monitor(2,"ACER", 27, 1080, 13000, "Monitor Acer V246hql","./Multimedia/Monitor2.jpg",1)
+const monitor2 = new Monitor(2,"ACER", 27, 1080, 13000, "Monitor Acer V246hql","./Multimedia/Monitor2.jpg")
 Monitores.push(monitor2)
 
-const monitor3 = new Monitor(3,"SAMSUNG", 32, 1444, 15000, "Monitor Samsung LF27T","./Multimedia/Monitor3.jpg",1)
+const monitor3 = new Monitor(3,"SAMSUNG", 32, 1444, 15000, "Monitor Samsung LF27T","./Multimedia/Monitor3.jpg")
 Monitores.push(monitor3)
 
-const monitor4 = new Monitor(4,"VIEWSONIC", 27, 1444, 13000,"Monitor Viewsonic VX2468-PC" ,"./Multimedia/Monitor4.jpg",1)
+const monitor4 = new Monitor(4,"VIEWSONIC", 27, 1444, 13000,"Monitor Viewsonic VX2468-PC" ,"./Multimedia/Monitor4.jpg")
 Monitores.push(monitor4)
 
-const monitor5 = new Monitor(5,"GIGABYTE", 24, 720, 5000, "Monitor Gigabyte G24F","./Multimedia/Monitor5.jpg",1)
+const monitor5 = new Monitor(5,"GIGABYTE", 24, 720, 5000, "Monitor Gigabyte G24F","./Multimedia/Monitor5.jpg")
 Monitores.push(monitor5)
 
-const monitor6 = new Monitor(6,"ASUS", 27, 1444, 15000, "Monitor ASUS TUF ","./Multimedia/Monitor6.jpg",1)
+const monitor6 = new Monitor(6,"ASUS", 27, 1444, 15000, "Monitor ASUS TUF ","./Multimedia/Monitor6.jpg")
 Monitores.push(monitor6)
 
 //--------------------------------------Eventos------------------------------//
@@ -65,6 +64,9 @@ const botonDark = document.getElementById('botonDark')
 
 const eliminarProducto = document.getElementById('eliminarProducto')
 
+const confirmarCompra = document.getElementById('confirmarCompra')
+
+
 document.addEventListener('DOMContentLoaded' , () => {
   if (localStorage.getItem('carrito')){
     Carrito = JSON.parse(localStorage.getItem('carrito'))
@@ -72,9 +74,49 @@ document.addEventListener('DOMContentLoaded' , () => {
   }
 })
 
+confirmarCompra.addEventListener(`click` , () => {
+  
+  Swal.fire(
+    {
+      title: '¿Terminaste de agregar tus productos?',
+      text: 'Al confirmar se te llevara a otra seccion para finalizar la compra',
+      icon: 'sucefull',
+      showCancelButton: true,
+      confirmButtonText: '<a href="./formulario.html" class="confirmar"> Confirmar </a>',
+      cancelButtonText: 'Cancelar'
+    }
+  ).then( (result) => {
+    if(result.isConfirmed){
+      Carrito.length = 0
+      localStorage.setItem('carrito' , JSON.stringify(Carrito))
+    }
+  })
+})
+
 botonVaciar.addEventListener(`click` , () => {
-  Carrito.length = 0
-  actualizarCarrito()
+
+  Swal.fire(
+    {
+        title: '¿Está seguro que quiere vaciar el carrito?',
+        text: 'Su compra no se a realizado aun!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, vaciar',
+        cancelButtonText: 'No, cancelar'
+    }
+).then( (result) => {
+    if (result.isConfirmed) {
+    Carrito.length = 0
+    localStorage.setItem('carrito' , JSON.stringify(Carrito))
+  Toastify({
+    text: 'Se vació el carrito',
+    time: 3000
+}).showToast()
+
+actualizarCarrito()
+}
+} )
+  
 })
 
 abrirModal.addEventListener(`click` , () => {
@@ -85,22 +127,18 @@ cerrarModal.addEventListener(`click` , () => {
   containerModal.classList.remove('container-modal-active')
 })
 
-botonDark.addEventListener(`click` , () => {
-  header.classList.toggle('headerActive')
-  body.classList.toggle('bodyActive')
-  footer.classList.toggle('footerActive')
-})
+
 
 Monitores.forEach(producto => {
     const div = document.createElement('div')
     div.className =('claseProducto')
     div.innerHTML = `<h4 class="text-danger">${producto.nombre}</h4>
                     <img class="im" src=${producto.img}>
-                    <p>$${producto.precio}</p>
+                    <p class="clasePrecio">$${producto.precio}</p>
                     <div class ="botonesDeLosProductos">
                      <button class="btn btn-outline-success">+</button>
                      <button class="btn btn-outline-danger">-</button>
-                     <p id="cantidad">xxxx</p>
+                     <p>xxxx</p>
                     </div>
                     <button id="agregar${producto.id}" class="agregar">Agregar</button> `
 
@@ -115,26 +153,31 @@ Monitores.forEach(producto => {
     })
 })
 const agregarAlCarrito = (prodId) => {
-  const existe = Carrito.some (prod => prod.id === prodId)
+  const existe = Carrito.find( (prod) => prod.id === prodId)
 
   if(existe){
-    const prod = Carrito.map(prod => {
-      if(prod.id === prodId){
-        prod.cantidad++
-      }
-    })
+        existe.cantidad += 1
   }else{
     const item = Monitores.find ((prod) => prod.id === prodId)
-    Carrito.push(item)
+    Carrito.push({
+      ...item,cantidad: 1})
     console.log(Carrito)
   }
-    
+    localStorage.setItem('carrito', JSON.stringify(Carrito))
+    toastAgregar()
     actualizarCarrito()
   }
 const eliminarDelCarrito = (prodId) => {
   const item = Carrito.find((prod) => prod.id === prodId)
+
+  item.cantidad -= 1
+
+  if(item.cantidad === 0){
   const indice = Carrito.indexOf(item)
   Carrito.splice(indice,1)
+  }
+  localStorage.setItem('carrito', JSON.stringify(Carrito))
+  toastEliminar()
   actualizarCarrito()
 }
 
@@ -149,19 +192,38 @@ const actualizarCarrito = () => {
       <p>${prod.nombre}</p>
       <p>Precio: $${prod.precio}</p>
       <p>Cantidad: <span id ="cantidad">${prod.cantidad}</span></p>
-      <button id="eliminarProducto${prod.id}" onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"<i class="fas fa-trash-alt">Eliminar</button>
+      <button id="eliminarProducto${prod.id}" onclick="eliminarDelCarrito(${prod.id })" class="boton-eliminar"<i class="fas fa-trash-alt">Eliminar</button>
       `
-      
       contenedorCarrito.prepend(div)
   
-     localStorage.setItem('carrito', JSON.stringify(Carrito))
-
-     
     })
     contadorCarrito.innerText = Carrito.reduce((acc, prod) => acc + prod.cantidad ,0)
     precioTotal.innerText = Carrito.reduce((acc,prod) => acc + prod.precio * prod.cantidad,0 )
   }
 
   
+  const toastAgregar = () => {
+    Toastify({
+        text: 'Producto agregado al carrito',
+        time: 3000,
+        gravity: 'bottom',
+        position: 'right'
+    }).showToast()
+}
+const toastEliminar = () => {
+    Toastify({
+        text: 'Producto eliminado',
+        time: 3000,
+        gravity: 'bottom'
+    }).showToast()
+}
+const clasePrecio = document.querySelector (".clasePrecio")
 
+botonDark.addEventListener(`click` , () => {
+  header.classList.toggle('headerActive')
+  body.classList.toggle('bodyActive')
+  footer.classList.toggle('footerActive')
+  clasePrecio.classList.toggle('clasePrecioActive')
+})
+actualizarCarrito()
   
